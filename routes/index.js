b81a02ee8b6c597e0a7144980fe1f9b079a908c5
maydolensky <mdolensky@udesa.edu.ar>
2024-04-14 00:39:08 -0300

    ruta, controller y ejs de login

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,8 @@ router.get ("/",controllerGeneral.index) // en vez del controller redirige direc
 router.get ("/register",controllerGeneral.registro) // en vez del controller redirige direct a el ejs
 router.get ("/editarPerfil",controllerGeneral.editarPerfil) // en vez del controller redirige direct a el ejs
 router.get ("/resultadosBusqueda",controllerGeneral.resultados) // en vez del controller redirige direct a el ejs
+router.get ("/login",controllerGeneral.login) 
+
 
 
 
