d0e6c24bd0c6b8b74eb2a5547895fd569a14adc4
Markelesperanza <mesperanza@udesa.edu.ar>
2024-04-15 17:34:52 -0300

    producto

--- controllers/controller.js
+++ controllers/controller.js
@@ -27,6 +27,40 @@ const controllers= {
 
     login: function (req,res) {
         return res.render ('login')
+    },
+    
+    producto: function(req, res){
+        let id = req.params.id
+        let producto = []
+        let comentarios = []
+
+        for (let i = 0; i < dataBase.productos.length; i++) {
+            
+            if (id == dataBase.productos[i].id) {
+
+                let valor = dataBase.productos[i]
+
+                producto.push({
+                    nombreProducto: dataBase.productos[i].nombreProducto,
+                    descripcion: dataBase.productos[i].descripcion,
+                    imagen: dataBase.productos[i].imagen,
+                })
+
+                for (let i = 0; i < valor.comentarios.length; i++) {
+                    
+                    comentarios.push({
+                        id: valor.comentarios[i].id,
+                        nombreUsuario: valor.comentarios[i].nombreUsuario,
+                        comentario: valor.comentarios[i].comentario,
+                        imagenPerfil: valor.comentarios[i].imagenPerfil,
+                    }) 
+                }
+            }
+            
+        }
+        let productoFiltrado = producto[0]
+        console.log(comentarios);
+        return res.render('product', {producto: productoFiltrado, comentarios})
     }
     
 
