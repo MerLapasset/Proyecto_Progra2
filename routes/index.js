3effabcec3ff9ef61d038e5895bbd34f411ff4fa
maydolensky <mdolensky@udesa.edu.ar>
2024-04-22 18:19:46 -0300

    destaco hipervinvulos- done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 let express = require('express');
 let router = express.Router();
 let controllerGeneral= require ("../controllers/controllerProducto")
-let controllerUsuario= require ("../controllers/controllerUsuario")
 
 router.get ("/",controllerGeneral.index) 
 router.get("/product/:id", controllerGeneral.producto)
