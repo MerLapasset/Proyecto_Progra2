3538a7c2107813ea4d51cf5f010dd42279d2f371
Mercedes <mlapasset@udesa.edu.ar>
2024-04-21 23:30:33 -0300

    modificacion en el archivo rutas index (productos)

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,9 @@ let controllerUsuario= require ("../controllers/controllerUsuario")
 
 router.get ("/",controllerGeneral.index) 
 router.get("/product/:id", controllerGeneral.producto)
-
-
-//router.get ("/register", controllerUsuario.register) 
-//router.get ("/login", controllerUsuario.login) 
-//router.get ("/profileEdit", controllerUsuario.profileEdit) 
 router.get ("/searchResults", controllerGeneral.searchResults) 
 router.get ("/productAdd", controllerGeneral.productAdd) 
 
-//router.get("/profile", controllerUsuario.profile, controllerUsuario.misProductos)
 
 
 
