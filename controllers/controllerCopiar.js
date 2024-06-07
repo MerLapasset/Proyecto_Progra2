7b785e6e031900d9be4dc6896d17de93d3c4b50f
Markelesperanza <mesperanza@udesa.edu.ar>
2024-06-07 11:29:58 -0300

    Creacion de Sequelize, archivo database y cambios en el  archivo .gitignore

---
+++ controllers/controllerCopiar.js
@@ -0,0 +1,5 @@
+//const db = require('../database/models');
+//const movie = db.movie
+
+//Pegar en todos los controllers para usar la base da datos de Mysql
+//Se cambia movie por el nombre del archivo creado en la carpeta models
