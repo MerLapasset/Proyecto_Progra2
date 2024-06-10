a511d34866d996811ff2ec432244019a068037bf
maydolensky <mdolensky@udesa.edu.ar>
2024-06-10 14:54:40 -0300

    agregamos sequelize

--- database/models/User.js
+++ database/models/User.js
@@ -38,11 +38,25 @@ module.exports= function (sequeize, dataTypes) {
 
         },
 
+        deletedAt:{
+            type: dataTypes.DATE,
+            field: 'deletedAt'
+        },
+
         producto_id: {
             type: dataTypes.INTEGER,
         }
     };
     
+    let config = {
+        tableName : "productos", 
+        timestamps:true, //Aclareción en caso de no explicitar created_at, deleted_at y updated_at --> si, (uptadeAt)
+        underscored: true, //Aclareción en caso que los timestamps usen guiones bajos en lugar de camelCase. --> si tenemos (usuario_id)
+        createdAt: "created_at",
+        updatedAt: "updated_at",
+        deletedAt: "deleted_at"
+    };
+
     const Usuario = sequelize.define(alias, cols, config);
 
     Usuario.associate= function (models) {
